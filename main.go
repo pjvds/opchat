08abc6c2dde60955c84adba01e6e3adab99fee53
Pieter Joost van de Sande <pj@born2code.net>
2015-04-08 15:31:37 +0200

    accept messages and store them in mongo

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pjvds/tidy"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	log = tidy.GetLogger()
+)
+
+func main() {
+	dbUrl := "localhost"
+	session, err := mgo.Dial(dbUrl)
+
+	if err != nil {
+		log.With("url", dbUrl).Fatal(err.Error())
+	}
+
+	db := session.DB("opchat")
+	router := gin.Default()
+	log := tidy.GetLogger()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	router.POST("/thread/:thread_id", func(c *gin.Context) {
+		request := struct {
+			Message  string `json:"message" binding:"required"`
+			ByUserId string `json:"by_user_id" binding:"required"`
+			ToUserId string `json:"to_user_id" binding:"required"`
+			ThreadId string
+		}{
+			ThreadId: c.Params.ByName("thread_id"),
+		}
+		c.Bind(&request)
+
+		// TODO: validate
+		if len(request.ThreadId) == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "missing thread id",
+			})
+			return
+		}
+
+		if len(request.Message) == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "missing message",
+			})
+			return
+		}
+
+		if len(request.ToUserId) == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "missing to_user_id",
+			})
+			return
+		}
+		if len(request.ByUserId) == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "missing by_user_id",
+			})
+			return
+		}
+		err := db.C("threads").Insert(bson.M{
+			"thread_id":  request.ThreadId,
+			"message":    request.Message,
+			"by_user_id": request.ByUserId,
+			"to_user_id": request.ToUserId,
+			"create_at":  time.Now().UTC(),
+		})
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.String(http.StatusCreated, "created")
+	})
+
+	address := ":8080"
+
+	log.With("address", address).Info("started")
+	router.Run(":8080")
+}
